eaadb701c6bbb95b477ac2a44449110aa3e324f7
Franco Varela <a01199186@tec.mx>
2025-10-25 01:29:54 -0600

    Agregar .env al gitignore y arreglar tipos de Vite

--- firebase.ts
+++ firebase.ts
@@ -7,12 +7,18 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  // @ts-ignore
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDqMK7AZaRQyr6DvQwxOxuNJ7wtMxpoex8",
+  // @ts-ignore
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "pickpackpromx.firebaseapp.com",
+  // @ts-ignore
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "pickpackpromx",
+  // @ts-ignore
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "pickpackpromx.firebasestorage.app",
+  // @ts-ignore
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1072798208281",
+  // @ts-ignore
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:1072798208281:web:b818b9db86bd881430f588"
 };
 
 // Initialize Firebase
